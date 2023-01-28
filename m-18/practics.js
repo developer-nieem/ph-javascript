7e95ddbbf3ab50ee297e182510baeaa7bb4552a0
developer-nieem <nieemhossen@gmail.com>
2023-01-28 14:05:14 +0600

    module 18 practics part

---
+++ m-18/practics.js
@@ -0,0 +1,37 @@
+var myResult= 85;
+var tomResult = 66;
+var janeResult = 95;
+var petarResult = 56;
+var jhonResult = 40;
+
+var grades = tomResult;
+
+if(grades > 100){
+    console.log('Please Put your correct result');
+}else if(grades >= 80){
+    console.log('A');
+}else if(grades >= 60){
+    console.log('B');
+}else if(grades >= 50){
+    console.log('C');
+}else if(grades >= 40){
+    console.log('D');
+}else{
+    console.log('F');
+}
+
+
+
+
+
+
+// var side1 = 9 ;
+// var side2 = 8;
+// var side3 = 9;
+
+// if (side1 == side2 || side1==side3 || side2==side3 ) {
+    
+//     console.log('side1 and side3 are equal');
+// }else{
+//     console.log('not equal');
+// }
