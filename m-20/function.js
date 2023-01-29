cae679c645be7d84fb3c122d35f6f4b6d1c4485a
developer-nieem <naimhossenwork@gmail.com>
2023-01-29 23:02:24 +0600

    module 20 added and practics function, parameter, return

---
+++ m-20/function.js
@@ -0,0 +1,18 @@
+
+function laptopOpen(){
+
+    console.log('go to the laptop');
+    console.log('find power button');
+    console.log('click the button');
+
+}
+
+laptopOpen();
+console.log("---------------");
+laptopOpen();
+console.log("---------------");
+
+
+// diffarance betowen loop and function 
+// loop run at the same time fully
+// function you need to declared 
