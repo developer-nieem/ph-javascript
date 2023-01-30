95ff30c4b36770c76524f71b19e157e7d3b6a2e0
developer-nieem <naimhossenwork@gmail.com>
2023-01-30 15:13:44 +0600

    object and properties

---
+++ m-20/object.js
@@ -0,0 +1,21 @@
+
+var classes = {
+    department : "economics",
+    students :  400,
+}
+
+var computer = {
+    brabd : "Dell",
+    price : 40000,
+    ram : "8gb",
+    ssd : "256gb",
+    processor : "i5"
+}
+
+console.log(computer);
+
+console.log("ram koto ?", computer.ram);
+
+computer.ssd = "500gb";
+
+console.log(computer);
