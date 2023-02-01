9385785bf153eee87528810ba2840aa3b7b6a406
developer-nieem <naimhossenwork@gmail.com>
2023-02-01 19:13:04 +0600

    solved 25days hotel price

--- m-21.5/problem-solving.js
+++ m-21.5/problem-solving.js
@@ -14,13 +14,53 @@ console.log(output );
  */
 
 //  ======== f to c
-
+/*  
 function fahrenheitToCelsius(num){
-    var result = (num - 32) * 5/9    /* 0.555 */
+    var result = (num - 32) * 5/9    
    var resultFixed = result.toFixed(3);
     return resultFixed + "°C"
 }
 
 var output = fahrenheitToCelsius(40);
 
+console.log(output); 
+ 
+ */
+
+function hotel(day){
+    if(day <= 10){
+        var cost = day * 500;
+        return cost;
+    }else if(day >= 11 && day <= 20){
+        /* 
+        // cost 10days
+        var cost10Days = 10*500;
+
+        var daysAfter10 = day - 10;
+        var cost11To20= daysAfter10 * 300;
+        var total = (cost10Days + cost11To20) ;
+        return total;
+ */
+            return (day - 10)*300 + (500*10);
+
+    }else if(day >= 21){
+        /* 
+        // cost 10days
+        var cost10Days = 10*500;
+
+        // cost 11-20 dyas
+        var cost11To20 = 10*300;
+
+        var daysAfter20 = day -20;
+        var cost21ToInfinity= daysAfter20*100;
+        var total3 = cost10Days + cost11To20 + cost21ToInfinity; 
+        return total3 ;
+        */
+
+            return (10*500) + (10*300) + (day-20)*100;
+        
+    }
+}
+
+var output= hotel(18);
 console.log(output);
