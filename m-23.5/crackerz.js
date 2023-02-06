fabde0478940356f1a49f51cefe622e944a6fa44
developer-nieem <naimhossenwork@gmail.com>
2023-02-06 21:50:39 +0600

    crackerz solving

--- m-23.5/crackerz.js
+++ m-23.5/crackerz.js
@@ -7,7 +7,7 @@ const reverseMaker = (word)=> {
 let input = reverseMaker("GeeksforGeeks");
 
  */
-
+// ====================================
 /* 
 const solution = (letter)=> {
     if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
@@ -21,7 +21,8 @@ const solution = (letter)=> {
   console.log(output);
 
    */
-
+  // ====================================
+/* 
   const totalCost = (products) => {
 
     let sum = 0;
@@ -34,4 +35,43 @@ return sum;
 
   let output = totalCost([{ name: "Samsung M33", price: 10000 },{ name: "Samsung Charger", price: 500},{ name: "Haveit Speakers", price:2500}])
 
-  console.log(output);
+  console.log(output); */
+// ====================================
+ 
+/*   function temperatureConverter(valNum) {
+   let convert =  (valNum-32)/1.8;
+   const result = parseFloat(convert.toFixed(2));
+    return result;
+  }
+  temperatureConverter(103) */
+
+// ====================================
+
+ /*  function subtraction (num1, num2){
+            let result = num1 - num2;
+            return result;
+    }
+    console.log(subtraction(1000,700)); */
+
+    // ========================================
+/* 
+    function average (arrOfMarks){
+        let sum = 0;
+         
+            for (let i = 0; i < arrOfMarks.length; i++) {
+                const element = arrOfMarks[i];
+                sum = sum + element;
+                
+            }
+            const average = sum / arrOfMarks.length;
+             const result =parseFloat(average.toFixed(2))
+            return result;
+       }
+
+    let output =average( [100, 100, 100, 100, 100]);
+    console.log(output);
+     */
+
+    function stringCombine(str1, str2){
+         return str1 + " " + str2;
+        }
