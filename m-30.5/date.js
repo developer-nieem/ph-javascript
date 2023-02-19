53b84f742e9245f08bdf185f7344e2f62f23da36
developer-nieem <naimhossenwork@gmail.com>
2023-02-19 08:15:41 +0600

    practics modules 30.5

---
+++ m-30.5/date.js
@@ -0,0 +1,9 @@
+
+const time1 =  new Date(2022-12-12);
+const time2 =  new Date(2022-12-12);
+
+if(time1.getDate() === time2.getDate){
+    console.log('yesss its same');
+}else{
+    console.log('not same');
+}
---
+++ m-30.5/error.js
@@ -0,0 +1,8 @@
+try {
+    console.log(main);
+    const name  =  'nieem';
+} catch (error) {
+    console.log('error');
+}
+
+console.log('500000000000000');
