debc1ce241496a0c890e96f8b168a5ea70054897
developer-nieem <nieemhossen@gmail.com>
2023-03-08 18:42:56 +0600

    added range store

--- m-39/number-count.js
+++ m-39/number-count.js
@@ -1,17 +1,35 @@
 
 const showNumberCount = document.getElementById('show-count');
 
+let count = 0;
 document.getElementById('start').addEventListener('click', function(){
-    let count = 0;
+    
    const intervalId = setInterval(() =>{
         showNumberCount.innerText = ++count;
-
-        document.getElementById('stop').addEventListener('click', function(){
-            clearInterval(intervalId);
-        })
-    }, 1000)
+        
+    }, 1000);
+    document.getElementById('stop').addEventListener('click', function(){
+        clearInterval(intervalId);
+    })
 })
 
 document.getElementById('reset').addEventListener('click', function(){
     showNumberCount.innerText =  0;
 })
+
+// let count =  0 ;
+// let id ;
+
+// document.getElementById('start').addEventListener('click', function(){
+// id = setInterval(() => {
+//     showNumberCount.innerText = count;
+//     count++;
+// }, 1000);
+
+// })
+
+// document.getElementById('stop').addEventListener('click', function(){
+// clearInterval(id);
+
+// })
+
